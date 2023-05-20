303b869d37c9152447f37df3db215b8b1a39f9ed
Mario Nakazato <marionakazat@gmail.com>
2023-05-20 18:20:03 -0300

    Creating chat

--- app/src/main/java/com/nak/fakeblock/MainActivity.java
+++ app/src/main/java/com/nak/fakeblock/MainActivity.java
@@ -1,14 +1,167 @@
 package com.nak.fakeblock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.nak.fakeblock.models.Message;
+import com.nak.fakeblock.models.User;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MESSAGES_CHILD = "chat";
+
+    private DatabaseReference mFirebaseDatabaseReference;
+    private FirebaseRecyclerAdapter<Message, MessageViewHolder> mFirebaseAdapter;
+
+    private Button mSendButton;
+    private RecyclerView mMessageRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private EditText mMessageEditText;
+    private String mEmail = "Anonymous";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = findViewById(R.id.editTextMessage);
+        mMessageRecyclerView = findViewById(R.id.RecyclerViewMessage);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager.setStackFromEnd(true);
+
+        // Inicializar Firebase Auth
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mFirebaseDatabaseReference.child("users").child(mFirebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                mEmail = user.email;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+        Query query = mFirebaseDatabaseReference.child(MESSAGES_CHILD);
+        FirebaseRecyclerOptions<Message> options = new FirebaseRecyclerOptions.Builder<Message>()
+                .setQuery(query, Message.class)
+                .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Message, MessageViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(MessageViewHolder viewHolder, int position, Message message) {
+                if (message.getUsername().equals(mEmail)) {
+                    viewHolder.row.setGravity(Gravity.END);
+                } else {
+                    viewHolder.row.setGravity(Gravity.START);
+                }
+                viewHolder.messageTextView.setText(message.getText());
+                viewHolder.messengerTextView.setText(message.getUsername());
+            }
+
+            @Override
+            public MessageViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                return new MessageViewHolder(inflater.inflate(R.layout.component_message, viewGroup, false));
+            }
+        };
+
+        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                int friendlyMessageCount = mFirebaseAdapter.getItemCount();
+                int lastVisiblePosition = mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
+                // Se a exibição do reciclador estiver sendo carregada inicialmente ou o usuário estiver no final da lista, role.
+                // Parte inferior da lista para mostrar a mensagem recém-adicionada.
+                if (lastVisiblePosition == -1 || (positionStart >= (friendlyMessageCount - 1) && lastVisiblePosition == (positionStart - 1))) {
+                    mMessageRecyclerView.scrollToPosition(positionStart);
+                }
+            }
+        });
+        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
+
+        mMessageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        mSendButton = findViewById(R.id.buttonSend);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Message message = new Message(mMessageEditText.getText().toString(), mEmail);
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(message);
+                mMessageEditText.setText("");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mFirebaseAdapter != null) {
+            mFirebaseAdapter.startListening();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mFirebaseAdapter != null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout row;
+        TextView messageTextView;
+        TextView messengerTextView;
+
+        MessageViewHolder(View v) {
+            super(v);
+            row = itemView.findViewById(R.id.row);
+            messageTextView = itemView.findViewById(R.id.textViewMessage);
+            messengerTextView = itemView.findViewById(R.id.textViewMessenger);
+        }
     }
 }
---
+++ app/src/main/java/com/nak/fakeblock/models/Message.java
@@ -0,0 +1,30 @@
+package com.nak.fakeblock.models;
+
+public class Message {
+    private String text;
+    private String username;
+
+    public Message() {
+    }
+
+    public Message(String text, String username) {
+        this.text = text;
+        this.username = username;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
